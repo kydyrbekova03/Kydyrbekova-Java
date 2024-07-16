812478102e2d38f78698f2772bcce099e044d8b5
Kaniet Kydyrbekova <kydyrbekova995@gmail.com>
2024-07-16 13:38:06 +0600

    1st task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner enteredNum = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+
+        int number = enteredNum.nextInt();
+        if (number > 7) {
+            System.out.println("Привет");
+        }
+
+
+        }
+
     }
-}
+
